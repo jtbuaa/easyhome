dbe70ba7fa466029678a390bcfa6b2e9ce825894
danny <danny@danny-Latitude-E6400.(none)>
2013-05-01 11:24:46 +0800

    add project files

---
+++ src/simple/home/jtbuaa/FakeHome.java
@@ -0,0 +1,12 @@
+package simple.home.jtbuaa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FakeHome extends Activity {
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+	}
+}
